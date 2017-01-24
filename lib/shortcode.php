258a7deda767f27fc41664933379e32bf01faec8
Rafael Nowrotek <rafaelnowrotek@rafaels-mbp-2.localdomain>
2017-01-24 14:00:23 +0100

    Fix options

--- lib/shortcode.php
+++ lib/shortcode.php
@@ -1,37 +1,38 @@
 <?php
 function findus_shortcode($atts = array(), $content = "") {
-  
+
   $findus_options = array('content', 'address', 'longitude', 'latitude', 'map', 'marker', 'info');
   $atts = shortcode_atts(array(
     'id' => uniqid(),
-    //'class' => '',
-    //'address' => '',
-    //'content' => '',
-    //'latitude' => '',
-    //'longitude' => '',
-    //'info' => ''
+    'class' => '',
+    'address' => '',
+    'content' => '',
+    'latitude' => '',
+    'longitude' => '',
+    'info' => ''
   ), $atts, 'findus');
-  
+
   $atts['class'] = trim('findus ' . (isset($atts['class']) ? $atts['class'] : ''));
-  
+
   $options = array();
   foreach ($atts as $name => $value) {
-    if (in_array($name, $findus_options) === true) {
-      unset($atts[$name]);
+    if (in_array($name, $findus_options)) {
       $options[$name] = $value;
-    } 
+      unset($atts[$name]);
+    }
   }
+
   $json = json_encode($options, JSON_UNESCAPED_SLASHES);
-  
+
   // Tidy up html
   $content = force_balance_tags($content);
-  
+
   // Remove empty paragraphs and trim content
   $content = trim(preg_replace('~<p>\s*(<\s*br\s*/?\s*>|&nbsp;)*\s*</p>~si', '', $content));
-  
+
   // Strip root node if it's a paragraph
   //$content = preg_replace('~^<p>(.+?(?=</p>))</p>$~si', '$1', $content);
-  
+
   // Create output
   $output = "<div";
   foreach ($atts as $name => $value) {
@@ -43,8 +44,8 @@ function findus_shortcode($atts = array(), $content = "") {
   $output.= "<script type=\"text/javascript\">//<![CDATA[\n(function($, window) {\n";
   $output.= "\t$('#{$atts['id']}').findus(" . $json . ");\n";
   $output.= "})(jQuery, window)\n//]]></script>\n";
-  
+
   return $output;
 }
- 
+
 add_shortcode('findus', 'findus_shortcode');
