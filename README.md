e03ddea23f1ade92433a10a41a1ad5d32e12986a
Rafael Nowrotek <rafaelnowrotek@Rafaels-MacBook-Pro-2.local>
2019-07-17 12:04:35 +0200

    Add demo theme and styles example

--- README.md
+++ README.md
@@ -68,3 +68,75 @@ Simply drag the FindUs-Widget to your sidebar and enter title and some geocodeab
     <td>longitude</td><td>Location coordinate longitude</td><td></td>
   </tr>
 </table>
+
+
+## Development
+
+Download [Docker CE](https://www.docker.com/get-docker) for your OS.
+Download [NodeJS](https://nodejs.org) for your OS.
+
+### Install
+
+#### Install wordpress
+
+```cli
+docker-compose run --rm wp install-wp
+```
+
+After installation you can log in with user `wordpress` and password `wordpress`.
+
+#### Install front-end dependencies
+
+```cli
+npm i
+```
+
+### Development Server
+
+Point terminal to your project root and start up the container.
+
+```cli
+docker-compose up -d
+```
+
+Point your browser to [http://localhost:8030](http://localhost:8030).
+
+
+#### Watch front-end dependencies
+
+```cli
+npm run watch
+```
+
+### Docker
+
+##### Update composer dependencies
+
+```cli
+docker-compose run composer update
+```
+
+##### Globally stop all running docker containers
+
+```cli
+docker stop $(docker ps -a -q)
+```
+
+##### Update Wordpress
+
+Due to some permission issues, you need to chmod your container's web-root prior to running the updater:
+
+```cli
+docker-compose exec wordpress bash
+```
+
+From the container shell, change permissons all down the tree.
+```cli
+chmod -R 777 .
+```
+
+After `CTRL+D`, you're ready to update Wordpress, either from the admin-interface or using wp-cli:
+
+```
+docker-compose run wp core update
+```
---
+++ test/fixtures/themes/findus/functions.php
@@ -0,0 +1,69 @@
+<?php
+
+// Enqueue Scripts
+add_action('wp_enqueue_scripts', function() {
+  wp_register_style('twentyseventeen-style', get_template_directory_uri(). '/style.css');
+  wp_enqueue_style('twentyseventeen-style', get_template_directory_uri(). '/style.css');
+  wp_enqueue_style( 'childtheme-style', get_stylesheet_directory_uri().'/style.css', array('twentyseventeen-style') );
+  wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
+  wp_enqueue_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array( 'jquery' ), '', true);
+  wp_enqueue_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', array( 'jquery' ), '', true);
+  wp_enqueue_script('turbolinks', 'https://cdn.jsdelivr.net/npm/turbolinks@5.2.0/dist/turbolinks.min.js', null, '', false);
+});
+
+// Init bootstrap hooks
+if (function_exists('wp_bootstrap_hooks')) {
+  wp_bootstrap_hooks();
+}
+
+// Customize Maps
+add_filter('shortcode_atts_findus', function($out, $pairs, $atts, $shortcode) {
+  return array_merge($out, array(
+    'map' => array(
+      'styles' => array (
+        0 =>
+        array (
+          'featureType' => 'all',
+          'stylers' =>
+          array (
+            0 =>
+            array (
+              'color' => '#C0C0C0',
+            ),
+          ),
+        ),
+        1 =>
+        array (
+          'featureType' => 'road.arterial',
+          'elementType' => 'geometry',
+          'stylers' =>
+          array (
+            0 =>
+            array (
+              'color' => '#CCFFFF',
+            ),
+          ),
+        ),
+        2 =>
+        array (
+          'featureType' => 'landscape',
+          'elementType' => 'labels',
+          'stylers' =>
+          array (
+            0 =>
+            array (
+              'visibility' => 'off',
+            ),
+          ),
+        ),
+      )
+    )
+  ), $atts);
+}, 10, 4);
+
+// Setup phpmailer
+add_action( 'phpmailer_init', function($phpmailer) {
+  $phpmailer->Host = 'mailhog';
+  $phpmailer->Port = 1025;
+  $phpmailer->IsSMTP();
+});
